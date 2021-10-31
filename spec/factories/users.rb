d52e23c422b026ad6dbe6f3418a6c8857e709acf
Marco McNulty <marcomcnultycodes@gmail.com>
2021-10-31 15:11:31 +0000

    📦 NEW: Users scaffold

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+
+    factory :physician_user do
+      role { 'physician' }
+    end
+  end
+end
